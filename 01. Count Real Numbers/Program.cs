24bf72cc94e446331b5ef62ee7bd5436dc1bb971
Emberclap <133802297+Emberclap@users.noreply.github.com>
2023-06-29 14:24:19 +0300

    Add project files.

---
+++ 01. Count Real Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _01._Count_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+
+            SortedDictionary<double, int> keyValuePairs = new SortedDictionary<double, int>();
+            int count = 1;
+            foreach (double number in numbers)
+            {
+                if (keyValuePairs.ContainsKey(number))
+                {
+                    keyValuePairs[number]++;
+                }
+                else
+                {
+                    keyValuePairs.Add(number, 1);
+                }
+            }
+            foreach (var number in keyValuePairs)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}
---
+++ 3. Word Synonyms/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _3._Word_Synonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var words = new Dictionary<string,  List<string>>();
+            
+            
+            for (int i = 0; i < n; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+                if (!words.ContainsKey(word))
+                {
+                    words.Add(word, new List<string>());
+                }
+                    words[word].Add(synonym);
+                
+            }
+            foreach (var keyValuePair  in words)
+            {
+                Console.WriteLine($"{keyValuePair.Key} - {string.Join(", ", keyValuePair.Value)}");
+            }
+        }
+    }
+}
